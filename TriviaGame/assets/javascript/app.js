e6ba9fadc302c5a6b848a4016ef6c6e5ac093ce2
RacquelO <RacquelO@users.noreply.github.com>
2016-03-05 10:20:28 -0500

    trivia

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -3,24 +3,25 @@ $(document).ready(function(){
 var questions = {
 
     q1:{
-      Question: "New Orleans is known as the birthplace of what type of music?",
-      Correct: "Jazz",
-      options: ["Jazz","Rock","R&B","Pop"],
-    },
+      Question: "New Orleans is known as the birthplace of what type of music?"
+      Correct: "Jazz"
+      options: ["Jazz","Rock","R&B","Pop"]
+    }
     q2:{
-      Question: "Who was the lead singer of the rock band Queen?",
-      Correct: "Freddie Mercury",
-      options: ["Freddie Mercury","Paul McCartney","Jimi Hendrix","Bob Dylan"],
-    },
+      Question: "Who was the lead singer of the rock band Queen?"
+      Correct: "Freddie Mercury"
+      options: ["Freddie Mercury","Paul McCartney","Jimi Hendrix","Bob Dylan"]
+    }
     q3:{
-      Question:"What was the first music video played on MTV?",
-      Correct:"Video Killed the Radio Star",
-      options:["Video Killed the Radio Star","Billie Jean","Wake Me Up Before You Go-Go","Listen To Your Heart"],
-    },
+      Question:"What was the first music video played on MTV?"
+      Correct:"Video Killed the Radio Star"
+      options:["Video Killed the Radio Star","Billie Jean","Wake Me Up Before You Go-Go","Listen To Your Heart"]
+    }
     q4:{
-      Question:"Which famous American musician was fatally shot by his father on April 1, 1984?",
-      Correct:"Marvin Gaye",
-      options:["Marvin Gaye","Roy Orbison","Tom Evans","Felix Pappalardi"],
-    },
+      Question:"Which famous American musician was fatally shot by his father on April 1, 1984?"
+      Correct:"Marvin Gaye"
+      options:["Marvin Gaye","Roy Orbison","Tom Evans","Felix Pappalardi"]
+    }
   }
+})
 
