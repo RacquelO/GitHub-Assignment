bbd9b34f9d8e7b614bf453a4140f84c0b662579a
RacquelO <RacquelO@users.noreply.github.com>
2016-05-05 18:28:42 -0400

    week_13

---
+++ FriendFinder/app/routing/api-routes.js
@@ -0,0 +1,33 @@
+// Your `api-routes.js` file should include two routes:
+// 	* A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends
+// 	* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+
+
+	// API GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
+	// ---------------------------------------------------------------------------
+
+	// //when my app is asked to get '/api/tables'...
+	// app.get('/api/friends', function(req, res){
+	// 	res.json(tableData);
+	// });
+
+
+	// // API POST Requests
+	// // Below code handles when a user submits a form and thus submits data to the server.
+	// // In each of the below cases, when a user submits form data (a JSON object)
+	// // ...the JSON is pushed to the appropriate Javascript array
+	// // (ex. User fills out a reservation request... this data is then sent to the server...
+	// // Then the server saves the data to the tableData array)
+	// // ---------------------------------------------------------------------------
+
+	// //when my app is asked to POST to /api/tables...
+	// app.post('/api/friends', function(req, res){
+	// 	console.log(req.body);
+
+	// });
+
+	console.log("test 2");
---
+++ FriendFinder/app/routing/html-routes.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+// var path = require('path');
+
+
+// // Routes
+// // =============================================================
+// module.exports = function(app){
+
+// 	// Each of the below routes just handles the HTML page that the user gets sent to.
+// 	app.use('/survey', function(req, res){
+// 		res.sendFile(path.join(__dirname + 'public/survey.html'));
+// 	});
+
+
+// 	// If no matching route is found default to home
+// 	app.use('/', function(req, res){
+// 		res.sendFile(path.join(__dirname + 'public/home.html'));
+// 	});
+
+// }
+console.log('test 1');
